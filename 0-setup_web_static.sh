7420f08d4511de97e16815f7eea4345e210298b6
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-12-15 16:36:23 +0300

    task 1 completed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt install -y nginx
-sudo ufw allow 80
-sudp ufw reload 
+sudo apt-get install -y nginx
+sudo ufw allow "Nginx HTTP"
+sudo ufw reload
 #this variable holds ngnix default file path
 nginx_conf="/etc/nginx/sites-available/default"
 #this will make the folder /data/web_static/releases/test
@@ -10,8 +10,12 @@ mkdir -p /data/web_static/releases/test
 #this will create index file and add test case in it
 echo "<html><head></head><body>Holberton School</body></html>" |  sudo tee /data/web_static/releases/test/index.html
 #this create smbolic link
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+#change owner
+sudo chown -hR ubuntu:ubuntu /data
 #this line of code will copy the file
 sudo sed -i "/server_name/a \\n\t location /hbnb_static {\\n\talias /data/web_static/current;}" "${nginx_conf}"
+#create smbolic link for nginx
+sudo ln -sf "${nginx_conf}" /etc/nginx/sites-enabled/default
 #restarting nginx
 sudo service nginx restart
