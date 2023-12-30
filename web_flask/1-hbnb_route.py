82e7e9eef7ce3988b1b490de4b53448712e53f95
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-12-31 00:48:33 +0300

    task 1 completed

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+this file contains the url when http://0.0.0.0:5000/ or curl 0.0.0.0:5000
+entered it will respond
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """this will be called when the above url entered
+
+    Returns:
+        str: this will return string value
+    """
+    return "{}".format("Hello HBNB!")
+
+@app.route("/hbnb")
+def hbnb():
+    """this will be called when http://0.0.0.0:5000/hbnb or
+       curl 0.0.0.0:5000/hbnb url entered
+
+    Returns:
+        str: this will return string value
+    """
+    return "{}".format("HBNB")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
